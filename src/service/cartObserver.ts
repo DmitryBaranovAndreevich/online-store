b312028ddc00c0f98f16f8d296a9314175f4af17
Harry177 <harry177@rambler.ru>
2022-12-29 01:11:08 +0100

    fix: renew cartObserver.ts

--- src/service/cartObserver.ts
+++ src/service/cartObserver.ts
@@ -1,23 +1,15 @@
 import { GoodList } from "../components/goodsList/goodsList";
+import { IGood } from "../interface/good";
 import { Cart } from "../pages/cart";
 import { goods } from "./goods";
 
-interface IArray {
-  id: number;
-  volume: number;
-}
-
 export class CartObserver {
-  state: Array<IArray> = []; // здесь будем хранить состояние корзины
-  subscribers: GoodList | Cart | null; // корзина и блок товаров - это разные страницы, соответственно мы не никогда не сможем здесь иметь
-  // одновременно 2 класса , либо один, либо другой, при открытии каждой страницы каждый раз здесь будет новый обьект
+  state: { [key: string]: number } = {}; // здесь будем хранить состояние корзины
+  private subscribers: GoodList | Cart | null = null; // корзина и блок товаров - это разные страницы, соответственно мы не никогда не сможем здесь иметь
+  // одновременно 2 класса , либо один, либо другой, приоткрытии каждой страницы каждый раз здесь будет новый обьект
   constructor() {
-    this.subscribers = null;
-    if (localStorage.getItem("item") !== null) {
-      this.state = JSON.parse(localStorage.getItem("item") as string) as IArray[];
-    } else {
-      this.state = [];
-    }
+    this.state = JSON.parse(localStorage.getItem("item") as string) as { [key: string]: number };
+    console.log(this.state);
   }
 
   public subscribe(obj: GoodList | Cart) {
@@ -25,41 +17,35 @@ export class CartObserver {
     this.subscribers = obj;
   }
 
-  /*public addElement(id: number) {
-    this.state.push(); // добавляем элемент в
+  public addElement(id: number) {
+    this.state = { ...this.state, [id]: 1 };
     this.notify();
-  }*/
+  }
 
   public increase(id: number) {
-    this.state = this.state.map((good) => {
-      console.log(good.id === id);
-      return good.id === id ? { ...good, volume: ++good.volume } : good;
-    }); // увеличиваем элемент с нужным id на 1
+    this.state = { ...this.state, [id]: this.state[id] + 1 };
     this.notify();
   }
 
   public decrease(id: number) {
-    this.state = this.state.map((good) => {
-      console.log(good.id === id);
-      return good.id === id ? { ...good, volume: --good.volume } : good;
-    });
-    this.state.forEach((good) => {
-      if (good.volume < 1) {
-        this.state.splice(this.state.indexOf(good), 1);
-      }
-    });
-    // если volume > 1 то уменьшаем, иначе удаляем
+    console.log("tets");
+    if (this.state[id] === 1) {
+      delete this.state[id];
+    } else {
+      this.state = { ...this.state, [id]: this.state[id] - 1 };
+    }
     this.notify();
   }
 
   public notify() {
-    localStorage.setItem("item", JSON.stringify(this.state)); // добавляем массив в локал сторедж
-    if (this.subscribers !== null) {
-      // если this.subscribers !== null, то вызываем у него метод перерисовки, соответственно каждому классу нужно добавить метод , котрый удалит все товары и отрисует их заново
-      this.subscribers.updateRender(goods.products);
-    }
-    // у goodsList такой метод есть  - updateRender добавь такой же и для своего
     console.log(this.state);
+    localStorage.setItem("item", JSON.stringify(this.state));
+    const renderArr = Array.from(Object.keys(this.state)).map((id) => {
+      const good = goods.products.find((good) => good.id === Number(id)) as IGood;
+      return { ...good, volume: this.state[id] };
+    });
+    console.log(this.subscribers instanceof GoodList, renderArr);
+    if (this.subscribers instanceof GoodList) this.subscribers.updateRender();
+    else this.subscribers?.updateRender(renderArr);
   }
 }
-// для GoodList и Cart надо добавить поля, которые будут инициализироваться в конструкторе и получать из локал сторедж наш state: Array<IArray>
